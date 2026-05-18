277f0e8a690ab2544a682d68784224d2564b5d5b
Casebook Killer <casebookkiller@yandex.ru>
2026-05-18 09:18:57 +0300

    Step 8: Finalize Vite and Forge configuration

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,12 +1,29 @@
 import path from "path";
 import { defineConfig } from "vite";
 
+export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "dist/main"),
+    lib: {
+      entry: path.resolve(__dirname, "src/preload.ts"),
+      formats: ["cjs"],
+      fileName: () => "preload.js",
+    },
+    emptyOutDir: false,
+    rollupOptions: {
+      external: ["electron"],
+    },
+  },
+});
+/*import path from "path";
+import { defineConfig } from "vite";
+
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
     outDir: path.resolve(__dirname, "dist/main"), // путь вывода для preload-процесса
   },
-});
+});*/
 
 
 /*
